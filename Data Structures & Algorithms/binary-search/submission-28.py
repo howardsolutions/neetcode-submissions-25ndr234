0d64070d9d8206edbfd91df838c5b805f5203fb8
Howard <64355894+howardsolutions@users.noreply.github.com>
2026-06-20 12:15:13 +0700

    Add: binary-search - submission-28

---
+++ Data Structures & Algorithms/binary-search/submission-28.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+
+            potential = nums[m]
+
+            if (potential == target):
+                return m
+
+            if potential > target: 
+                r = m - 1
+            elif potential < target:
+                l = m + 1 
+
+        return -1
