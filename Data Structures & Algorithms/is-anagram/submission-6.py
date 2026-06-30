55cb02889b4cb950e1b00e55c51a87d964b78a0c
Howard <64355894+howardsolutions@users.noreply.github.com>
2026-06-30 15:48:12 +0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            # everytime we see a character in the string, we want to increment the count BY 1
+            # the .get() method because, s[i], or t[i] could be null at first, default the value to 0
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        return countS == countT
